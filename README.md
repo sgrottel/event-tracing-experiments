c12136bc8777c560b149ed98492452940072b842
Sebastian Grottel <sebastian.grottel@microsoft.com>
2022-03-31 11:50:24 +0200

    Windows event tracing added to RollingDice and documentation.

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# event-tracing-experiments
+# Event Tracing Experiments
 Windows Event Tracing Experiments
+
+## Rolling Dice
+This is a little example console application.
+It rolls 2x W6 dice every second, prints the result on the console output and traces the result.
+
+
+## Windows Performance Recorder
+To record a event trace file you can use the [Windows Performance Recorder](https://docs.microsoft.com/en-us/windows-hardware/test/wpt/windows-performance-recorder).
+
+From an admin command prompt run:
+```PS
+wpr -start .\diceprofile.wprp -filemode
+```
+to start the tracing.
+
+Then run the Rolling Dice application.
+
+To stop the tracing and write the trace file run: (admin command prompt)
+```PS
+wpr -stop trace.etl
+```
+This will write the file `trace.etl`.
+You can open the file, for example, with the [Windows Performance Analyzer](https://docs.microsoft.com/en-us/windows-hardware/test/wpt/windows-performance-analyzer).
+
+
+## License
+The these experimental tools are available freely under the terms of the Apache License, V.2
+
+> Copyright 2022 S.Grottel
+>
+> Licensed under the Apache License, Version 2.0 (the "License");
+> you may not use this file except in compliance with the License.
+> You may obtain a copy of the License at
+>
+> http://www.apache.org/licenses/LICENSE-2.0
+>
+> Unless required by applicable law or agreed to in writing, software
+> distributed under the License is distributed on an "AS IS" BASIS,
+> WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+> See the License for the specific language governing permissions and
+> limitations under the License.
+
+For details, also see [LICENSE](./LICENSE).
--- RollingDice/RollingDice.cpp
+++ RollingDice/RollingDice.cpp
@@ -10,10 +10,26 @@
 
 #include <conio.h>
 
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <winmeta.h>
+#include <TraceLoggingProvider.h>
+
+TRACELOGGING_DECLARE_PROVIDER(g_traceloggingProvider);
+
+// https://docs.microsoft.com/en-us/windows/win32/api/traceloggingprovider/nf-traceloggingprovider-tracelogging_define_provider
+// [System.Diagnostics.Tracing.EventSource]::new("RollingDice").Guid
+TRACELOGGING_DEFINE_PROVIDER(g_traceloggingProvider,
+    "RollingDice",
+    (0x0d6910e0, 0xb0fe, 0x5ee8, 0x00, 0xe3, 0xc6, 0xbb, 0x1c, 0x97, 0x9a, 0x53));
+
 int main()
 {
     std::cout << "Rolling Dice (2x W6)\n"
         << "Press any key to exit\n";
+
+    TraceLoggingRegister(g_traceloggingProvider);
 
     std::atomic<bool> running{ true };
     auto runner = std::async(
@@ -22,7 +38,7 @@ int main()
             constexpr const int sleepSteps = 10;
             constexpr const std::chrono::milliseconds sleepDur{ 1000 / sleepSteps };
 
-            std::default_random_engine eng{ std::chrono::system_clock::now().time_since_epoch().count() };
+            std::default_random_engine eng{ static_cast<unsigned int>(std::chrono::system_clock::now().time_since_epoch().count()) };
             std::uniform_int_distribution<int> distr{ 1, 6 };
 
             while (running) {
@@ -34,7 +50,18 @@ int main()
                 int die1 = distr(eng);
                 int die2 = distr(eng);
 
-                std::cout << "\t" << die1 << " " << die2 << "    (" << (die1 + die2) << ")" << std::endl;
+                int sum = die1 + die2;
+
+                std::cout << "\t" << die1 << " " << die2 << "    (" << sum << ")" << std::endl;
+
+                TraceLoggingWrite(g_traceloggingProvider,
+                    "Rolling Dice",
+                    TraceLoggingDescription("Rolled 2x W6 dice"),
+                    TraceLoggingInt32(die1, "Die1", "First die"),
+                    TraceLoggingInt32(die2, "Die2", "Second die"),
+                    TraceLoggingInt32(sum, "Dice", "Summary value of dice")
+                );
+
             }
         }
     );
@@ -47,4 +74,6 @@ int main()
     runner.wait();
 
     std::cout << "Stopped  " << std::endl;
+
+    TraceLoggingUnregister(g_traceloggingProvider);
 }
